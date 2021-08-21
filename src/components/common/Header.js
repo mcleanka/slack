4c1984406052846a01c197873f503ed955d482e9
mcleanka <mcleankasambala@gmail.com>
2021-08-21 10:04:56 +0200

    Add:: added styled components

---
+++ src/components/common/Header.js
@@ -0,0 +1,25 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+	return (
+		<HeaderContainer>
+			<HeaderLeft>
+				<HeaderAvatar
+
+				/>
+			</HeaderLeft>
+
+			{/* Search */}
+
+			{/* Header Right */}
+		</HeaderContainer>
+	)
+}
+
+export default Header;
+
+const HeaderContainer = styled.div``
+const HeaderLeft = styled.div``
+const HeaderAvatar = styled(Avatar)``
---
+++ src/config/firebase.js
@@ -0,0 +1,22 @@
+
+// Your web app's Firebase configuration
+
+var firebaseConfig = {
+
+	apiKey: "AIzaSyAOYkOPQzhbyyhCg48zUw--AUY1cNGGe9Q",
+
+	authDomain: "slack-app-a93ca.firebaseapp.com",
+
+	projectId: "slack-app-a93ca",
+
+	storageBucket: "slack-app-a93ca.appspot.com",
+
+	messagingSenderId: "766521279595",
+
+	appId: "1:766521279595:web:1709f2028449ba1fa51d2e"
+
+};
+
+// Initialize Firebase
+
+// firebase.initializeApp(firebaseConfig);
